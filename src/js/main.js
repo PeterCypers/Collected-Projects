bf600f4a943e149fe85ca793b5721b36ee45a699
PeterCypers <peter-cypers@hotmail.com>
2025-05-05 00:15:17 +0200

    started on the main content

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+
+// pretending we're in React: (1)
+// document.addEventListener("DOMContentLoaded", () => {
+//   fetch('components/card.html')
+//     .then(response => response.text())
+//     .then(html => {
+//       document.getElementById('card-container').innerHTML = html;
+//     })
+//     .catch(err => console.error("Failed to load component:", err));
+// });
